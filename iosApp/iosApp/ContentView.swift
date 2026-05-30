5f89383f7c3954b26dd966705505fc77b6cafd7b
Manish <manish.k@samparksoftwares.com>
2026-05-30 16:14:51 +0530

    Expose state flow data from viewmodel to ios native using skie plugin

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,8 +2,26 @@ import SwiftUI
 import SharedLogic
 
 struct ContentView: View {
+    
+    @State private var shouldOpenAbout = false
+    
     var body: some View {
-       AboutScreen()
+        NavigationStack{
+            ArticlesScreen(viewModel: .init())
+                .toolbar {
+                    ToolbarItem{
+                        Button{
+                            shouldOpenAbout = true
+                        } label: {
+                            Label("About", systemImage: "info.circle").labelStyle(.titleAndIcon)
+                        }
+                        .popover(isPresented: $shouldOpenAbout){
+                            AboutScreen()
+                        }
+                    }
+                }
+        }
+      
     }
 }
 
--- sharedLogic/build.gradle.kts
+++ sharedLogic/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidMultiplatformLibrary)
+    id("co.touchlab.skie") version "0.10.12"
 }
 
 kotlin {
